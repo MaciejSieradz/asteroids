f1456a2b04a01a51e7cd1f41d9425803c53e8e93
Maciej Sieradz <macieks53@gmail.com>
2025-01-07 16:29:58 +0100

    Added player

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from pygame.color import Color
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color = Color(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
